ce58fc63795489f16e85f7f6da4a2770e200d8bd
Rhyian Joshua Ticbobolan <ticbobolanrhyianjoshua18@gmail.com>
2026-05-14 01:28:17 +0800

    Resolve merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -10,16 +9,3 @@ export default defineConfig({
     setupFiles: "./src/test/setup.js",
   },
 });
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/test/setup.js",
-  },
-});
->>>>>>> 535af6926ae60f228da74f82990a30ff8a584b19
